281b24b34ed1402e7bd4b342eb6d0fe450f49642
FakeScoutBot <132735283+FakeScoutBot@users.noreply.github.com>
2025-04-16 20:58:28 +0530

    Add files via upload

---
+++ StringGen/utils/inline.py
@@ -0,0 +1,27 @@
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+from config import SUPPORT_CHAT
+
+
+keyboard = InlineKeyboardMarkup(
+    [
+        [InlineKeyboardButton(text="ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ", callback_data="gensession")],
+        [
+            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=SUPPORT_CHAT),
+            InlineKeyboardButton(
+                text="ᴄʜᴀɴɴᴇʟ", url="https://t.me/+v1ubYvri73owZDk9"
+            ),
+        ],
+    ]
+)
+
+gen_key = InlineKeyboardMarkup(
+    [
+        [InlineKeyboardButton(text="ᴩʏʀᴏɢʀᴀᴍ", callback_data="pyrogram")],
+        [InlineKeyboardButton(text="ᴛᴇʟᴇᴛʜᴏɴ", callback_data="telethon")],
+    ]
+)
+
+retry_key = InlineKeyboardMarkup(
+    [[InlineKeyboardButton(text="ᴛʀʏ ᴀɢᴀɪɴ", callback_data="gensession")]]
+)
