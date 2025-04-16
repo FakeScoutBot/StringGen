9e72493b2db9315ef23fa47eba00b231a9cb61cd
FakeScoutBot <132735283+FakeScoutBot@users.noreply.github.com>
2025-04-16 22:11:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,4 +12,4 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 OWNER_ID = int(getenv("OWNER_ID", 6878311635))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+v1ubYvri73owZDk9")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+rFrVYWJbaCsxNzRl")
